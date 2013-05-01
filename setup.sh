4d9b50892c84d4798983bcaf01ed04629ace7b3b
Leo Lai <leo@ubuntu.(none)>
2013-05-01 02:01:27 -0700

    Add sudo.

--- setup.sh
+++ setup.sh
@@ -9,15 +9,15 @@ sudo apt-get install curl
 
 sudo apt-get --no-install-recommends install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev
 
-\curl -L https://get.rvm.io | bash -s stable
+sudo \curl -L https://get.rvm.io | bash -s stable
 
-source ~/.rvm/scripts/rvm
-rvm requirements
+sudo source ~/.rvm/scripts/rvm
+sudo rvm requirements
 
-rvm install ree-1.8.7
+sudo rvm install ree-1.8.7
 
-rvm use ree-1.8.7 --default
+sudo rvm use ree-1.8.7 --default
 
-gem install --no-rdoc --no-ri chef
+sudo gem install --no-rdoc --no-ri chef
 
 
