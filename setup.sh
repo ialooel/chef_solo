f9e24df9649ed6de61f49fa7ef243da08c3fde5b
Leo Lai <leooo.lai@gmail.com>
2013-05-01 02:15:18 -0700

    Update sudo.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install curl
 
 sudo apt-get --no-install-recommends install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev
 
-sudo \curl -L https://get.rvm.io | bash -s stable
+\curl -L https://get.rvm.io | sudo bash -s stable
 
 sudo source ~/.rvm/scripts/rvm
 sudo rvm requirements
