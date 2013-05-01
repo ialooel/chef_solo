f2419c19648084094274748d987044034d2475fb
Leo Lai <leo.lai@advanced-pay.com>
2013-05-01 21:34:31 +0800

    Not using RVM on setup.

--- setup.sh
+++ setup.sh
@@ -5,18 +5,19 @@
 # sudo su - 
 sudo apt-get update
 
-sudo apt-get install curl
-
 sudo apt-get --no-install-recommends install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev
 
-\curl -L https://get.rvm.io | sudo bash -s stable
+#
+# Install Rubygems, package management framework
+# Check here for latest version: http://rubygems.org/pages/download
+#
 
-sudo source ~/.rvm/scripts/rvm
-sudo rvm requirements
-
-sudo rvm install ree-1.8.7
-
-sudo rvm use ree-1.8.7 --default
+cd /tmp
+wget http://production.cf.rubygems.org/rubygems/rubygems-2.0.3.tgz
+tar zxf rubygems-2.0.3.tgz
+cd rubygems-2.0.3
+sudo ruby setup.rb --no-format-executable
+cd ~
 
 sudo gem install --no-rdoc --no-ri chef
 
