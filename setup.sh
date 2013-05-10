558b7ca2b73255bb5a45ec978d7a44f9fbdc0883
Leo Lai <leo.lai@advanced-pay.com>
2013-05-11 04:53:37 +0800

    Failed to try the ree187 way.

--- setup.sh
+++ setup.sh
@@ -4,19 +4,25 @@
 
 #
 # Set locale
-#
+# (file /etc/default/locale)
 sudo locale-gen en_US.UTF-8
 sudo /usr/sbin/update-locale LANG=en_US.UTF-8
 sudo dpkg-reconfigure locales
 
-# reboot here to update locales
+# ***** reboot here to update locales
 
 #
 # Install prerequisites
 #
 sudo apt-get update
 sudo apt-get --no-install-recommends install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev
-sudo apt-get install ruby ruby-dev libopenssl-ruby rdoc ri irb build-essential wget ssl-cert git-core
+sudo apt-get wget
+
+# ree 1.8.7, 32-bit version here.  Use 64-bit if suitable.
+sudo wget http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise_1.8.7-2012.02_i386_ubuntu10.04.deb
+sudo dpkg -i ruby-enterprise_1.8.7-2012.02_i386_ubuntu10.04.deb 
+
+sudo apt-get install ruby-dev libopenssl-ruby rdoc ri irb build-essential ssl-cert git-core
 
 #
 # Install Rubygems, package management framework
