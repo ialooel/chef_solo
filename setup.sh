8d9165134cf1c44700cf5fc3f0585e67173bbf22
Leo Lai <leo.lai@advanced-pay.com>
2013-05-13 07:24:28 +0800

    cont. on trying rvm approach.

--- setup.sh
+++ setup.sh
@@ -3,13 +3,27 @@
 #
 # This has to be run as root on server
 # sudo su - 
+
+# Step 1:
+sudo locale-gen en_US.UTF-8
+sudo /usr/sbin/update-locale LANG=en_US.UTF-8 
+sudo /usr/sbin/update-locale LC_ALL=en_US.UTF-8 
+sudo dpkg-reconfigure locales
+
+# Step 2:
+# Reboot
+
+# Step 3:
 sudo apt-get update
-
 sudo apt-get install curl
-
 sudo apt-get --no-install-recommends install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev libgdbm-dev ncurses-dev automake libtool bison subversion pkg-config libffi-dev
 
+# Step 4:
+# sudo-ed
 \curl -L https://get.rvm.io | sudo bash -s stable
+
+# Step 5:
+# open another bash.
 
 sudo source ~/.rvm/scripts/rvm
 sudo rvm requirements
